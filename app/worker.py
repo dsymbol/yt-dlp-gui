e081b588527f4c8fef9ddaac11d1c00b919a9608
dsymbol <dsymbol@users.noreply.github.com>
2024-08-31 20:38:26 +0300

    use shlex split instead of .split for cargs

--- app/worker.py
+++ app/worker.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import shlex
 import subprocess as sp
 import sys
 from dataclasses import dataclass
@@ -24,7 +25,19 @@ class Worker(qtc.QThread):
     finished = qtc.Signal(int)
     progress = qtc.Signal(object, list)
 
-    def __init__(self, item, link, path, format_, cargs, filename, sponsorblock, metadata, thumbnail, subtitles):
+    def __init__(
+        self,
+        item,
+        link,
+        path,
+        format_,
+        cargs,
+        filename,
+        sponsorblock,
+        metadata,
+        thumbnail,
+        subtitles,
+    ):
         super().__init__()
         self.item = item
         self.link = link
@@ -42,32 +55,48 @@ class Worker(qtc.QThread):
 
     def build_command(self):
         args = [
-            'yt-dlp', '--newline', '--ignore-errors', '--ignore-config', '--hls-prefer-native', '--no-simulate',
-            '--progress', '--progress-template', '%(progress.status)s %(progress._total_bytes_estimate_str)s '
-            '%(progress._percent_str)s %(progress._speed_str)s %(progress._eta_str)s', '--dump-json', '-v',
-            '-o', f'{self.path}/{self.filename}', self.link
+            "yt-dlp",
+            "--newline",
+            "--ignore-errors",
+            "--ignore-config",
+            "--no-simulate",
+            "--progress",
+            "--progress-template",
+            "%(progress.status)s %(progress._total_bytes_estimate_str)s "
+            "%(progress._percent_str)s %(progress._speed_str)s %(progress._eta_str)s",
+            "--dump-json",
+            "-v",
+            "-o",
+            f"{self.path}/{self.filename}",
+            self.link,
         ]
         if self.format == "best":
-            args += ['-f', r'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b']
+            args += ["-f", r"bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b"]
         elif self.format == "mp4":
-            args += ['-f', r'bv*[vcodec^=avc]+ba[ext=m4a]/b']
+            args += ["-f", r"bv*[vcodec^=avc]+ba[ext=m4a]/b"]
         else:
-            args += ['--extract-audio', '--audio-format', self.format, '--audio-quality', '0']
+            args += [
+                "--extract-audio",
+                "--audio-format",
+                self.format,
+                "--audio-quality",
+                "0",
+            ]
 
         if self.cargs:
-            args += self.cargs.split()
+            args += shlex.split(self.cargs)
         if self.metadata:
-            args += ['--embed-metadata']
+            args += ["--embed-metadata"]
         if self.thumbnail:
-            args += ['--embed-thumbnail']
+            args += ["--embed-thumbnail"]
         if self.subtitles:
-            args += ['--write-auto-subs']
+            args += ["--write-auto-subs"]
 
         if self.sponsorblock:
             if self.sponsorblock == "remove":
-                args += ['--sponsorblock-remove', 'all']
+                args += ["--sponsorblock-remove", "all"]
             else:
-                args += ['--sponsorblock-mark', 'all']
+                args += ["--sponsorblock-mark", "all"]
         return args
 
     def stop(self):
@@ -75,30 +104,35 @@ class Worker(qtc.QThread):
             self._stop = True
 
     def run(self):
-        create_window = sp.CREATE_NO_WINDOW if sys.platform == 'win32' else 0
+        create_window = sp.CREATE_NO_WINDOW if sys.platform == "win32" else 0
         command = self.build_command()
         error = False
 
-        with sp.Popen(command, stdout=sp.PIPE, stderr=sp.STDOUT, text=True,
-                      universal_newlines=True, creationflags=create_window) as p:
+        with sp.Popen(
+            command,
+            stdout=sp.PIPE,
+            stderr=sp.STDOUT,
+            text=True,
+            universal_newlines=True,
+            creationflags=create_window,
+        ) as p:
             for line in p.stdout:
                 with qtc.QMutexLocker(self.mutex):
                     if self._stop:
                         p.terminate()
                         break
 
-                if line.startswith('{'):
-                    info_dict = json.loads(line)
-                    title = info_dict['title']
-                    log.info(f"`{title}` with id {self.item.id} download started with args: " + " ".join(command))
+                if line.startswith("{"):
+                    title = json.loads(line)["title"]
+                    log.info(
+                        f"`{title}` with id {self.item.id} download started with args: "
+                        + shlex.join(command)
+                    )
                     self.progress.emit(
                         self.item,
-                        [
-                            [TreeDex.TITLE, title],
-                            [TreeDex.STATUS, "Processing"]
-                        ]
+                        [[TreeDex.TITLE, title], [TreeDex.STATUS, "Processing"]],
                     )
-                elif line.lower().startswith('downloading'):
+                elif line.lower().startswith("downloading"):
                     data = line.split()
                     self.progress.emit(
                         self.item,
@@ -107,10 +141,10 @@ class Worker(qtc.QThread):
                             [TreeDex.PROGRESS, data[2]],
                             [TreeDex.SPEED, data[3]],
                             [TreeDex.ETA, data[4]],
-                            [TreeDex.STATUS, "Downloading"]
-                        ]
+                            [TreeDex.STATUS, "Downloading"],
+                        ],
                     )
-                elif line.lower().startswith('error'):
+                elif line.lower().startswith("error"):
                     error = True
                     log.error(line)
                     self.progress.emit(
@@ -118,15 +152,12 @@ class Worker(qtc.QThread):
                         [
                             [TreeDex.SIZE, "ERROR"],
                             [TreeDex.STATUS, "ERROR"],
-                            [TreeDex.SPEED, "ERROR"]
-                        ]
+                            [TreeDex.SPEED, "ERROR"],
+                        ],
                     )
                     break
                 elif line.startswith(("[Merger]", "[ExtractAudio]")):
-                    self.progress.emit(
-                        self.item,
-                        [[TreeDex.STATUS, "Converting"]]
-                    )
+                    self.progress.emit(self.item, [[TreeDex.STATUS, "Converting"]])
 
             if not error:
                 self.progress.emit(
@@ -134,7 +165,7 @@ class Worker(qtc.QThread):
                     [
                         [TreeDex.PROGRESS, "100%"],
                         [TreeDex.STATUS, "Finished"],
-                    ]
+                    ],
                 )
 
         self.finished.emit(self.item.id)
